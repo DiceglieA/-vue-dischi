60a7b054942e93c9081ba34fc646b30d7b35c889
DiceglieA <angelodiceglie10@gmail.com>
2022-11-28 14:04:02 +0100

    header complete

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="header">
+      <img src="../assets/spotify.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  height: 10vh;
+  background-color: #2E3A46 ;
+  display: flex;
+  align-items: center;
+  text-align: start;
+  img{
+    width: 50px;
+    margin-left: 20px;
+  }
+}
+</style>
