035917a67fa5274ebc74efcfee3a89a4c83df7f9
DiceglieA <angelodiceglie10@gmail.com>
2022-11-28 16:22:02 +0100

    problemi con axios

---
+++ src/components/CardComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <img :src="card.poster" alt="">
+    </div>
+    <div>
+      <div>{{card.title}}</div>
+      <div>{{card.author}}</div>
+      <div>{{card.year}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComp',
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
+    <div >
+     <CardComp
+      v-for="(elem, index) in cards"
+      :key="index"
+      :card="elem"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import CardComp from '@/components/CardComp.vue';
+import axios from 'axios';
+
 export default {
   name: 'MainComp',
+  components: {
+    CardComp,
+  },
+  data() {
+    return {
+      cards: [],
+    };
+  },
+  created() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+        console.log(response);
+        this.cards = response.data.response;
+      });
+  },
 };
 </script>
 
